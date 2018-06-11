5883d80bdd807224b3ebcbfa49d07b1688b9b85f
Pieter Wuille <pieter.wuille@gmail.com>
2018-06-11 16:19:04 -0700

    Switch 1-way SSE4 SHA256 to intrinsics based implementation

--- src/crypto/sha256.cpp
+++ src/crypto/sha256.cpp
@@ -637,12 +637,9 @@ std::string SHA256AutoDetect(sha256_implementation::UseImplementation use_implem
 #endif
 
     if (have_sse4) {
-#if defined(__x86_64__) || defined(__amd64__)
-        Transform = sha256_sse4::Transform;
-        TransformD64 = TransformD64Wrapper<sha256_sse4::Transform>;
-        ret = "sse4(1way)";
-#endif
 #if defined(ENABLE_SSE41)
+        Transform = sha256_sse41::Transform;
+        TransformD64 = TransformD64Wrapper<sha256_sse41::Transform>;
         TransformD64_4way = sha256d64_sse41::Transform_4way;
         ret += ";sse41(4way)";
 #endif

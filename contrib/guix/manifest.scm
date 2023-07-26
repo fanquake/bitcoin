45680205bcbdff677876bdb18d72fe8d8d32efe4
fanquake <fanquake@gmail.com>
2023-07-26 13:55:00 +0100

    guix: add fortified glibc 2.40
    https://sourceware.org/git/?p=glibc.git;a=shortlog;h=refs/heads/release/2.40/master

--- contrib/guix/manifest.scm
+++ contrib/guix/manifest.scm
@@ -523,6 +523,37 @@ inspecting signatures in Mach-O binaries.")
     (description "Just sponge")
     (license license:gpl2+)))
 
+(define-public glibc-2.40
+  (let ((commit "c8e10f14328518954072df64aafd574e67cfdde5"))
+  (package
+    (inherit glibc) ;; 2.35
+    (version "2.40")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://sourceware.org/git/glibc.git")
+                    (commit commit)))
+              (file-name (git-file-name "glibc" commit))
+              (sha256
+               (base32
+                "0274sw502y0zl12wmx6ng52ymg9ydqsdxk06r1cnmbxi876b8wkz"))
+              (patches (search-our-patches "glibc-2.40-guix-prefix.patch"))))
+    (arguments
+      (substitute-keyword-arguments (package-arguments glibc)
+        ((#:configure-flags flags)
+          `(append ,flags
+            ;; https://www.gnu.org/software/libc/manual/html_node/Configuring-and-compiling.html
+            (list "--enable-stack-protector=all",
+                  "--enable-bind-now",
+                  "--enable-fortify-source",
+                  "--enable-cet=yes",
+                  "--enable-nscd=no",
+                  "--enable-static-nss=yes",
+                  "--disable-timezone-tools",
+                  "--disable-profile",
+                  "--disable-werror",
+                  building-on))))))))
+
 (packages->manifest
  (append
   (list ;; The Basics

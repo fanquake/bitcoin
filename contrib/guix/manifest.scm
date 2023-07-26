0f9930827c125f05d41d95925ad93f2597e17635
fanquake <fanquake@gmail.com>
2023-07-26 13:55:00 +0100

    guix: add fortified glibc 2.40

--- contrib/guix/manifest.scm
+++ contrib/guix/manifest.scm
@@ -97,7 +97,7 @@ chain for " target " development."))
                                        #:key
                                        (base-gcc-for-libc linux-base-gcc)
                                        (base-kernel-headers base-linux-kernel-headers)
-                                       (base-libc glibc-2.31)
+                                       (base-libc glibc-2.40)
                                        (base-gcc linux-base-gcc))
   "Convenience wrapper around MAKE-CROSS-TOOLCHAIN with default values
 desirable for building Bitcoin Core release binaries."
@@ -483,6 +483,36 @@ inspecting signatures in Mach-O binaries.")
                     (string-append out "/etc/rpc" suffix "\n"))
                    (("^install-others =.*$")
                     (string-append "install-others = " out "/etc/rpc\n")))))))))))))
+
+(define-public glibc-2.40
+  (let ((commit "f4a9b6e97bf05cf5a41907e55901f7e9afaafd4d"))
+  (package
+    (inherit glibc) ;; 2.39
+    (version "2.40")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://sourceware.org/git/glibc.git")
+                    (commit commit)))
+              (file-name (git-file-name "glibc" commit))
+              (sha256
+               (base32
+                "1rzdn7wy6asa22q9rwyizw3ha5xqpn193hwp2mmwdl87dr6yyksj"))
+              (patches (search-our-patches "glibc-2.40-guix-prefix.patch"))))
+    (arguments
+      (substitute-keyword-arguments (package-arguments glibc)
+        ((#:configure-flags flags)
+          `(append ,flags
+            ;; https://www.gnu.org/software/libc/manual/html_node/Configuring-and-compiling.html
+            (list "--enable-stack-protector=all",
+                  "--enable-bind-now",
+                  "--disable-werror",
+                  "--enable-fortify-source=yes",
+                  "--enable-cet=yes",
+                  "--enable-nscd=no",
+                  "--enable-static-nss=yes",
+                  "--enable-static-pie=yes",
+                  building-on))))))))
 
 (packages->manifest
  (append

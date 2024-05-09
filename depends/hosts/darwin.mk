6112b93614c6d3420498c6593b36e6461e66419c
fanquake <fanquake@gmail.com>
2024-05-09 19:20:17 +0800

    build: switch to using lld for macOS builds
    Adjust the security check for:
    ld64.lld: warning: Option `-allow_stack_execute' is not yet implemented.
    ld64.lld: error: -fixup_chains is incompatible with -no_pie
    and to account for the embedding of LLVMs version number.

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -106,7 +106,10 @@ darwin_LDFLAGS=-Wl,-platform_version,macos,$(OSX_MIN_VERSION),$(OSX_SDK_VERSION)
 ifneq ($(build_os),darwin)
 darwin_CFLAGS += -mlinker-version=$(LD64_VERSION)
 darwin_CXXFLAGS += -mlinker-version=$(LD64_VERSION)
-darwin_LDFLAGS += --ld-path=$(build_prefix)/bin/$(host)-ld
+darwin_LDFLAGS += -fuse-ld=lld
+ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
+darwin_LDFLAGS += --ld-path=$(build_prefix)/bin/ld64.lld
+endif
 endif
 
 darwin_release_CFLAGS=-O2

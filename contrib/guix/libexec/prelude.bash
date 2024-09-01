2ca26976b0a2a7ed72de9dc2f2b97900e5eb8dd2
fanquake <fanquake@gmail.com>
2024-09-01 09:26:33 +0100

    guix: update to 65d7be9eb95340324ad0f8cd813edb4d5ca21dd1
    Package updates:
    binutils 2.41 -> 2.44
    diffutils 3.10 -> 3.12
    file 5.45 -> 5.46
    git-minimal 2.46.0 -> 2.52.0
    (base) glibc 2.39 -> 2.41
    gzip 1.13 -> 1.14
    linux-headers 6.1.119 -> 6.1.164
    LLVM/Clang 19.1.4 -> 19.1.7
    mingw-w64 12.0.0 -> 13.0.0
    nsis 3.10 -> 3.11
    python-minimal 3.10 -> 3.11
    
    CMake 4.x becomes available.
    Clang/LLVM 20 & 21 become available.

--- contrib/guix/libexec/prelude.bash
+++ contrib/guix/libexec/prelude.bash
@@ -71,7 +71,7 @@ fi
 time-machine() {
     # shellcheck disable=SC2086
     guix time-machine --url=https://codeberg.org/guix/guix.git \
-                      --commit=5cb84f2013c5b1e48a7d0e617032266f1e6059e2 \
+                      --commit=65d7be9eb95340324ad0f8cd813edb4d5ca21dd1 \
                       --cores="$JOBS" \
                       --keep-failed \
                       --fallback \

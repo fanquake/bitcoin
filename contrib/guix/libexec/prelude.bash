0d02ea3a5e593be5c050edf8ad156b2bbd020728
fanquake <fanquake@gmail.com>
2024-09-01 09:26:33 +0100

    [WIP] guix: update to 9ef5533123bc2394b02fbbeaca1a5f50773b796d
    Currently, this causes atleast the riscv64 build to fail. glibc fails to
    build with the new binutils (2.41).
    
    Package updates:
    file 5.44 -> 5.45
    (base) glibc 2.35 -> 2.39
    grep 3.8 -> 3.11
    diffutils 3.8 -> 3.10
    gawk 5.2.1 -> 5.3.0
    gzip 1.12 -> 1.13
    xz 5.2.8 -> 5.4.5
    make 4.3 -> 4.4.1
    binutils 2.38 -> 2.41
    linux-headers 6.1.106 -> 6.1.119
    
    CMake-3.30 becomes available.
    Clang/LLVM 19 becomes available.

--- contrib/guix/libexec/prelude.bash
+++ contrib/guix/libexec/prelude.bash
@@ -51,7 +51,7 @@ fi
 time-machine() {
     # shellcheck disable=SC2086
     guix time-machine --url=https://git.savannah.gnu.org/git/guix.git \
-                      --commit=53396a22afc04536ddf75d8f82ad2eafa5082725 \
+                      --commit=9ef5533123bc2394b02fbbeaca1a5f50773b796d \
                       --cores="$JOBS" \
                       --keep-failed \
                       --fallback \

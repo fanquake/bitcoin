ea8d585b546e339332e23df89bbdbd087721ce23
fanquake <fanquake@gmail.com>
2024-09-01 09:26:33 +0100

    guix: update to 4f0b03a5dd8f4bb4ecf530359ae960c19dd38968
    Package updates:
    file 5.44 -> 5.45
    (base) glibc 2.35 -> 2.39
    grep 3.8 -> 3.11
    diffutils 3.8 -> 3.10
    gawk 5.2.1 -> 5.3.0
    gzip 1.12 -> 1.13
    xz 5.2.8 -> 5.4.5
    make 4.3 -> 4.4.1
    git-minimal 2.45.2 -> 2.48.1
    binutils 2.38 -> 2.41
    linux-headers 6.1.106 -> 6.1.128
    
    CMake-3.30 becomes available.
    Clang/LLVM 19 becomes available.

--- contrib/guix/libexec/prelude.bash
+++ contrib/guix/libexec/prelude.bash
@@ -51,7 +51,7 @@ fi
 time-machine() {
     # shellcheck disable=SC2086
     guix time-machine --url=https://git.savannah.gnu.org/git/guix.git \
-                      --commit=53396a22afc04536ddf75d8f82ad2eafa5082725 \
+                      --commit=4f0b03a5dd8f4bb4ecf530359ae960c19dd38968 \
                       --cores="$JOBS" \
                       --keep-failed \
                       --fallback \

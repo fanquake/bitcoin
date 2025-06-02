0c55e4b5223d7da4b44554c9cea10d10933b141f
fanquake <fanquake@gmail.com>
2025-06-02 11:56:03 +0100

    guix: switch back to Guix python-lief package

--- contrib/guix/manifest.scm
+++ contrib/guix/manifest.scm
@@ -17,6 +17,7 @@
              ((gnu packages python) #:select (python-minimal))
              ((gnu packages python-build) #:select (python-tomli python-poetry-core))
              ((gnu packages python-crypto) #:select (python-asn1crypto))
+             ((gnu packages python-xyz) #:select (python-lief))
              ((gnu packages tls) #:select (openssl))
              ((gnu packages version-control) #:select (git-minimal))
              (guix build-system cmake)
@@ -150,51 +151,6 @@ desirable for building Bitcoin Core release binaries."
 chain for " target " development."))
       (home-page (package-home-page pthreads-xgcc))
       (license (package-license pthreads-xgcc)))))
-
-;; While LIEF is packaged in Guix, we maintain our own package,
-;; to simplify building, and more easily apply updates.
-;; Moreover, the Guix's package uses cmake, which caused build
-;; failure; see https://github.com/bitcoin/bitcoin/pull/27296.
-(define-public python-lief
-  (package
-    (name "python-lief")
-    (version "0.13.2")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://github.com/lief-project/LIEF")
-                    (commit version)))
-              (file-name (git-file-name name version))
-              (modules '((guix build utils)))
-              (snippet
-               '(begin
-                  ;; Configure build for Python bindings.
-                  (substitute* "api/python/config-default.toml"
-                    (("(ninja         = )true" all m)
-                     (string-append m "false"))
-                    (("(parallel-jobs = )0" all m)
-                     (string-append m (number->string (parallel-job-count)))))))
-              (sha256
-               (base32
-                "0y48x358ppig5xp97ahcphfipx7cg9chldj2q5zrmn610fmi4zll"))))
-    (build-system python-build-system)
-    (native-inputs (list cmake-minimal python-tomli))
-    (arguments
-     (list
-      #:tests? #f                  ;needs network
-      #:phases #~(modify-phases %standard-phases
-                   (add-before 'build 'change-directory
-                     (lambda _
-                       (chdir "api/python")))
-                   (replace 'build
-                     (lambda _
-                       (invoke "python" "setup.py" "build"))))))
-    (home-page "https://github.com/lief-project/LIEF")
-    (synopsis "Library to instrument executable formats")
-    (description
-     "@code{python-lief} is a cross platform library which can parse, modify
-and abstract ELF, PE and MachO formats.")
-    (license license:asl2.0)))
 
 (define osslsigncode
   (package

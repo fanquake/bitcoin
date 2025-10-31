1b78477f3b481d71c2c6bd78fd66415b6a26cd2a
fanquake <fanquake@gmail.com>
2025-10-31 13:05:34 +0000

    ci: build Valgrind from source

--- ci/test/00_setup_env_native_fuzz_with_valgrind.sh
+++ ci/test/00_setup_env_native_fuzz_with_valgrind.sh
@@ -8,11 +8,12 @@ export LC_ALL=C.UTF-8
 
 export CI_IMAGE_NAME_TAG="mirror.gcr.io/ubuntu:24.04"
 export CONTAINER_NAME=ci_native_fuzz_valgrind
-export PACKAGES="libevent-dev libboost-dev libsqlite3-dev valgrind libcapnp-dev capnproto"
+export PACKAGES="libevent-dev libboost-dev libsqlite3-dev libcapnp-dev capnproto"
 export NO_DEPENDS=1
 export RUN_UNIT_TESTS=false
 export RUN_FUNCTIONAL_TESTS=false
 export RUN_FUZZ_TESTS=true
+export BUILD_VALGRIND=1
 export FUZZ_TESTS_CONFIG="--valgrind"
 export GOAL="all"
 export BITCOIN_CONFIG="\

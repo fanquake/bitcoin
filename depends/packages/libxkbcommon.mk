a00e89418b2a670928521563325d64678ec5beb4
fanquake <fanquake@gmail.com>
2025-11-17 13:32:56 +0000

    depends: libxkbcommon 1.3.1

--- depends/packages/libxkbcommon.mk
+++ depends/packages/libxkbcommon.mk
@@ -1,34 +1,24 @@
 package=libxkbcommon
-$(package)_version=0.8.4
-$(package)_download_path=https://xkbcommon.org/download/
-$(package)_file_name=$(package)-$($(package)_version).tar.xz
-$(package)_sha256_hash=60ddcff932b7fd352752d51a5c4f04f3d0403230a584df9a2e0d5ed87c486c8b
-$(package)_dependencies=libxcb
-
-# This package explicitly enables -Werror=array-bounds, which causes build failures
-# with GCC 12.1+. Work around that by turning errors back into warnings.
-# This workaround would be dropped if the package was updated, as that would require
-# a different build system (Meson)
-define $(package)_set_vars
-$(package)_config_opts = --enable-option-checking --disable-dependency-tracking
-$(package)_config_opts += --disable-shared --disable-docs
-$(package)_cflags += -Wno-error=array-bounds
-endef
-
-define $(package)_preprocess_cmds
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub build-aux
-endef
+$(package)_version=1.3.1
+$(package)_download_path=https://github.com/xkbcommon/$(package)/archive/refs/tags/
+$(package)_file_name=xkbcommon-$($(package)_version).tar.gz
+$(package)_sha256_hash=8eda6782c6ed4b83296521f2f7e6bea88aba76d49c39fb4fce0f8d355a9181ce
+$(package)_dependencies=libxcb libXau
 
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  meson setup build \
+    -Denable-docs=false \
+    -Denable-wayland=false \
+    -Denable-x11=true \
+    -Denable-xkbregistry=false
 endef
 
 define $(package)_build_cmds
-  $(MAKE)
+  meson compile -C build
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+  DESTDIR=$($(package)_staging_dir) meson install -C build
 endef
 
 define $(package)_postprocess_cmds

945fe39915a4057e37a08209b3668f60a331e571
fanquake <fanquake@gmail.com>
2025-12-04 14:21:55 +0000

    minify2

--- test.cpp
+++ test.cpp
@@ -2,7 +2,6 @@
 // ./contrib/guix/guix-build
 #include <filesystem>
 #include <string>
-#include <fstream>
 #include <vector>
 
 #include <cstring> // Unused, but removing this makes it determinstic
@@ -22,9 +21,7 @@ public:
 
 }
 
-bool IsBDBFile(const fs::path& path);
-
-bool IsSQLiteFile(const fs::path& path);
+bool func();
 
 std::vector<std::pair<fs::path, std::string>> ListDatabases(const fs::path& wallet_dir)
 {
@@ -34,22 +31,22 @@ std::vector<std::pair<fs::path, std::string>> ListDatabases(const fs::path& wall
     for (auto it = fs::recursive_directory_iterator(wallet_dir, ec); it != fs::recursive_directory_iterator(); it.increment(ec)) {
         const fs::path path{it->path().lexically_relative(wallet_dir)};
 
-        if (it->status().type() == fs::file_type::directory) {
-            if (IsBDBFile(it->path())) {
+        if (func()) {
+            if (func()) {
                 paths.emplace_back(path, "bdb");
-            } else if (IsSQLiteFile(it->path())) {
+            } else if (func()) {
                 paths.emplace_back(path, "sqlite");
             }
-        } else if (it.depth() == 0 && it->symlink_status().type() == fs::file_type::regular) {
-            if (it->path().filename() == "wallet.dat") {
-                if (IsBDBFile(it->path())) {
-                    paths.emplace_back(fs::path(), "bdb");
-                } else if (IsSQLiteFile(it->path())) {
-                    paths.emplace_back(fs::path(), "sqlite");
-                }
-            } else if (IsBDBFile(it->path())) {
-                paths.emplace_back(path, "bdb");
+        }
+        if (func()) {
+            if (func()) {
+                paths.emplace_back(fs::path(), "bdb");
+            } else if (func()) {
+                paths.emplace_back(fs::path(), "sqlite");
             }
+        } else if (func()) {
+            paths.emplace_back(path, "bdb");
+            paths.emplace_back(path, "wallet.dat");
         }
     }
 

4833022ae1859684ff99e709c9683f705f579c60
fanquake <fanquake@gmail.com>
2026-05-21 14:49:17 +0100

    guix: build arm-linux bitcoind fully statically
    -static-pie
    Produce a static position independent executable on targets that support it.
    A static position independent executable is similar to a static executable,
    but can be loaded at any address without a dynamic linker.
    
    See https://gcc.gnu.org/onlinedocs/gcc/Link-Options.html

--- contrib/guix/linux_static.scm
+++ contrib/guix/linux_static.scm
@@ -9,7 +9,8 @@
   (let ((target (getenv "HOST")))
     (cond ((or (string-contains target "x86_64-linux-")
                (string-contains target "riscv64-linux-")
-               (string-contains target "aarch64-linux-"))
+               (string-contains target "aarch64-linux-")
+               (string-contains target "arm-linux-"))
            (list (make-bitcoin-cross-toolchain target
                                                #:base-libc glibc-2.44)))
           ((string-contains target "-linux-")

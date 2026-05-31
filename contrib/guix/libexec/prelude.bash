efdfa5927092b0d015dbb6ba0fc209fe26560aad
fanquake <fanquake@gmail.com>
2026-05-31 20:17:04 +0100

    guix: update time-machine to 5842d6cf9b87add4cff12cbed7cba96358b10925
    git-minimal 2.52.0 -> 2.54.0
    python-lief 0.17.5 -> 0.17.6
    python-minimal 3.11.14 -> 3.12.12

--- contrib/guix/libexec/prelude.bash
+++ contrib/guix/libexec/prelude.bash
@@ -68,7 +68,7 @@ fi
 time-machine() {
     # shellcheck disable=SC2086
     guix time-machine --url=https://codeberg.org/guix/guix.git \
-                      --commit=c5eee3336cc1d10a3cc1c97fde2809c3451624d3 \
+                      --commit=5842d6cf9b87add4cff12cbed7cba96358b10925 \
                       --cores="$JOBS" \
                       --keep-failed \
                       --fallback \
